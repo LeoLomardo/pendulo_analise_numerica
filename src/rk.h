baa89a4ed5520d0a278faada43ce2fec588e9275
Leo Lomardo <leoland771@gmail.com>
2025-07-06 17:40:24 +0000

    fixed_header
    fix:add function headers to rk.h file

--- src/rk.h
+++ src/rk.h
@@ -11,4 +11,13 @@ int RungeKutta_system_adaptive_h(
     double tol, double h_initial,
     FILE *outfile, double y_final_out[]);
 
+void rk4_single_step_system(double t, const double y_in[], double h, int n_eq,
+                        void (*f)(double, double[], double[]),
+                        double y_out[]);
+
+int rk_adaptive_one_step(
+    double *t_current, double y_current[], double *h_current, int n_eq,
+    void (*f)(double, double[], double[]),
+    double tol, double h_min, double h_max);
 #endif
+
